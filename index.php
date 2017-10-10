e5c01ad76de6cff152bf9adb9e62176a3d1d04fc
Izzuddin Fauzi <izzuddinfz@gmail.com>
2017-10-10 11:44:40 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, 'http://live.mks.ch/mobile/myr/v1/');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_TIMEOUT,10);
+$response = curl_exec($ch);
+$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+curl_close($ch);
+
+if ($httpcode != '200'){
+    echo 'Bad Data';
+    echo '<br>';
+} else {
+    preg_match_all('/class=\"date\">(.*)<\/font>/', $response, $datematch);
+    preg_match_all('/right\">(.*)<\/td>/', $response, $datamatch);
+    
+    $curlDate = (string)str_replace(' CET           ', '', $datematch[1][0]);
+    
+    $dateGold = new DateTime($curlDate, new DateTimeZone('Europe/Andorra'));
+    $dateGold->setTimezone(new DateTimeZone('Asia/Kuala_Lumpur'));
+    $dateGold = $dateGold->format('d-m-Y H:i:s');
+    
+    $priceGold = number_format((float)str_replace(' ', '.', $datamatch[1][4]), 2, '.', '');
+    
+    echo 'Last Update '. $dateGold;
+    echo '<br>';
+    echo 'Sell Price RM'. $priceGold;
+    echo '<br>';
+    echo 'Buy Price RM'. $priceGoldSell;
+}
